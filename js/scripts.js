c6b23b92f3c0562a4c78e25916d0e23dd17bc18f
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-09-14 21:14:05 -0600

    Fix format

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,41 @@
 // array with Pokemon objects
-let pokemonList=[];
+let pokemonList = [];
 pokemonList = [
-    { 
+    {
         name: "Blastoise",
         type: ['Water'],
         height: 1.6,
     },
-    
-    { 
+
+    {
         name: "Umbreon",
         type: ['Dark'],
         height: 1,
     },
 
-    { 
+    {
         name: "Eevee",
         type: ['Normal'],
         height: 0.3,
     },
 
-    { 
+    {
         name: "Gengar",
-        type: ['Ghost','Poison'],
+        type: ['Ghost', 'Poison'],
         height: 1.5,
     }
 ]
 
+
 for (let i = 0; i < pokemonList.length; i++) {
     let pokemonName = pokemonList[i].name;
     let pokemonHeight = pokemonList[i].height;
-    
+
     let label = '';
-    
-    if (pokemonHeight > 1.5) {label = " - Wow that's a big Pokemon!";
+
+    if (pokemonHeight > 1.5) {
+        label = " - Wow that's a big Pokemon!";
     }
-  
-  document.write(`${pokemonName} (height: ${pokemonHeight}${label}) <br>`);
-  }
+
+    document.write(`${pokemonName} (height: ${pokemonHeight}${label}) <br>`);
+}
