1b8416f189da1e12c2a54f9ed3a9a65e60701b0c
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-09-19 21:56:36 -0600

    Add IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,32 @@
 // array with Pokemon objects
-let pokemonList = [];
-pokemonList = [
-    {
-        name: "Blastoise",
-        type: ['Water'],
-        height: 1.6,
-    },
+let pokemonRepository = (function () {
+    let pokemonList =
+        [
+            {
+                name: "Blastoise",
+                type: ['Water'],
+                height: 1.6,
+            },
 
-    {
-        name: "Umbreon",
-        type: ['Dark'],
-        height: 1,
-    },
+            {
+                name: "Umbreon",
+                type: ['Dark'],
+                height: 1,
+            },
 
-    {
-        name: "Eevee",
-        type: ['Normal'],
-        height: 0.3,
-    },
+            {
+                name: "Eevee",
+                type: ['Normal'],
+                height: 0.3,
+            },
 
-    {
-        name: "Gengar",
-        type: ['Ghost', 'Poison'],
-        height: 1.5,
+            {
+                name: "Gengar",
+                type: ['Ghost', 'Poison'],
+                height: 1.5,
+            }
+        ]
+
     function getAll() {
         return pokemonList;
     }
