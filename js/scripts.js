e8801526672d5603315aa889e2cf3d498b1049c6
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-09-21 16:04:05 -0600

    Change spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,49 @@
 // array with Pokemon objects
 let pokemonRepository = (function () {
-    let pokemonList =
-        [
-            {
-                name: "Blastoise",
-                type: ['Water'],
-                height: 1.6,
-            },
-
-            {
-                name: "Umbreon",
-                type: ['Dark'],
-                height: 1,
-            },
-
-            {
-                name: "Eevee",
-                type: ['Normal'],
-                height: 0.3,
-            },
-
-            {
-                name: "Gengar",
-                type: ['Ghost', 'Poison'],
-                height: 1.5,
-            }
-        ]
+    let pokemonList = [{
+          name: "Blastoise",
+          type: ['Water'],
+          height: 1.6,
+       },
+ 
+       {
+          name: "Umbreon",
+          type: ['Dark'],
+          height: 1,
+       },
+ 
+       {
+          name: "Eevee",
+          type: ['Normal'],
+          height: 0.3,
+       },
+ 
+       {
+          name: "Gengar",
+          type: ['Ghost', 'Poison'],
+          height: 1.5,
+       }
+    ]
+ 
     pokemonList.forEach(function (pokemon) {
-        console.log("Name:" + pokemon.name + " Type:" + pokemon.type + " Height:" + pokemon.height);
-        let label = "";
-        if (pokemon.height > 1.5) {
-            label = " - Wow that's a big Pokemon!";
-        }
-        document.write(`${pokemon.name} (height: ${pokemon.height}${label}) <br>`);
+       console.log("Name:" + pokemon.name + " Type:" + pokemon.type + " Height:" + pokemon.height);
+       let label = "";
+       if (pokemon.height > 1.5) {
+          label = " - Wow that's a big Pokemon!";
+       }
+       document.write(`${pokemon.name} (height: ${pokemon.height}${label}) <br>`);
     })
-
+ 
     function getAll() {
-        return pokemonList;
+       return pokemonList;
     }
-
+ 
     function add(pokemon) {
-        pokemonList.push(pokemon);
+       pokemonList.push(pokemon);
     }
     return {
-        getAll: getAll,
-        add: add
+       getAll: getAll,
+       add: add
     }
 
 })()
