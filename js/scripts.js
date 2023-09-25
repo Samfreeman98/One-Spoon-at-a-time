79a1a8d60297fbc001d2c0563e4eb3d5203db651
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-09-24 19:59:43 -0600

    Correct placement of forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,51 @@
 // array with Pokemon objects
 let pokemonRepository = (function () {
-    let pokemonList = [{
-          name: "Blastoise",
-          type: ['Water'],
-          height: 1.6,
-       },
- 
-       {
-          name: "Umbreon",
-          type: ['Dark'],
-          height: 1,
-       },
- 
-       {
-          name: "Eevee",
-          type: ['Normal'],
-          height: 0.3,
-       },
- 
-       {
-          name: "Gengar",
-          type: ['Ghost', 'Poison'],
-          height: 1.5,
-       }
-    ]
- 
-    pokemonList.forEach(function (pokemon) {
-       console.log("Name:" + pokemon.name + " Type:" + pokemon.type + " Height:" + pokemon.height);
-       let label = "";
-       if (pokemon.height > 1.5) {
-          label = " - Wow that's a big Pokemon!";
-       }
-       document.write(`${pokemon.name} (height: ${pokemon.height}${label}) <br>`);
-    })
- 
-    function getAll() {
-       return pokemonList;
-    }
- 
-    function add(pokemon) {
-       pokemonList.push(pokemon);
-    }
-    return {
-       getAll: getAll,
-       add: add
-    }
- 
- })()
- 
- 
- pokemonRepository.getAll().forEach(function (pokemon) {});
- 
+   let pokemonList = [{
+      name: "Blastoise",
+      type: ['Water'],
+      height: 1.6,
+   },
+
+   {
+      name: "Umbreon",
+      type: ['Dark'],
+      height: 1,
+   },
+
+   {
+      name: "Eevee",
+      type: ['Normal'],
+      height: 0.3,
+   },
+
+   {
+      name: "Gengar",
+      type: ['Ghost', 'Poison'],
+      height: 1.5,
+   }
+   ]
+
+   function getAll() {
+      return pokemonList;
+   }
+
+   function add(pokemon) {
+      pokemonList.push(pokemon);
+   }
+   return {
+      getAll: getAll,
+      add: add
+   }
+
+})()
+
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+   console.log("Name:" + pokemon.name + " Type:" + pokemon.type + " Height:" + pokemon.height);
+   let label = "";
+   if (pokemon.height > 1.5) {
+      label = " - Wow that's a big Pokemon!";
+   }
+   document.write(`${pokemon.name} (height: ${pokemon.height}${label}) <br>`);
+})
+
