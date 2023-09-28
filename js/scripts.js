d4c650ae0a1e84924abb90210db1706c09a8865a
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-09-28 12:35:31 -0600

    reformat file

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,76 @@
 let pokemonRepository = (function () {
-
-   //array of pokemon objects
-   let pokemonList = [{
-      name: "Blastoise",
+  //array of pokemon objects
+  let pokemonList = [
+    {
+      name: 'Blastoise',
       type: ['Water'],
-      height: 1.6,
-   },
+      height: 1.6
+    },
 
-   {
-      name: "Umbreon",
+    {
+      name: 'Umbreon',
       type: ['Dark'],
-      height: 1,
-   },
+      height: 1
+    },
 
-   {
-      name: "Eevee",
+    {
+      name: 'Eevee',
       type: ['Normal'],
-      height: 0.3,
-   },
+      height: 0.3
+    },
 
-   {
-      name: "Gengar",
+    {
+      name: 'Gengar',
       type: ['Ghost', 'Poison'],
-      height: 1.5,
-   }
-   ]
-
-   //function to return pokemonList-array
-   function getAll() {
-      return pokemonList;
-   }
-
-   function add(pokemon) {
-      pokemonList.push(pokemon);
-   }
-
-
-   function addListItem(pokemon) {
-      //selecting the pokemon-list
-      let pokemonList = document.querySelector(".pokemon-list");
-
-      //Creating variables for list items and buttons. 
-      let listItem = document.createElement("li");
-      let button = document.createElement("button");
-
-      //Button text is the name of the pokemon
-      button.innerText = pokemon.name;
-      //Class is added for CSS styling
-      button.classList.add("button-class");
-
-      //allows buttons and list to show on webpage
-      listItem.appendChild(button);
-      pokemonList.appendChild(listItem);
-
-      //When buttons are clicked the pokemons names are logged
-      button.addEventListener("click", function (event) {
          showDetails(pokemon.name);
-      });
-   }
-   function showDetails(pokemon) {
-      console.log(pokemon);
+      height: 1.5
+    }
+  ]
 
-   }
+  //function to return pokemonList-array
+  function getAll () {
+    return pokemonList
+  }
 
+  function add (pokemon) {
+    pokemonList.push(pokemon)
+  }
 
-   //Returns an object with all functions assigned as keys
-   return {
-      getAll: getAll,
-      add: add,
-      addListItem: addListItem,
-      showDetails: showDetails
-   };
+  function addListItem (pokemon) {
+    //selecting the pokemon-list
+    let pokemonList = document.querySelector('.pokemon-list')
 
-})();
+    //Creating variables for list items and buttons.
+    let listItem = document.createElement('li')
+    let button = document.createElement('button')
 
+    //Button text is the name of the pokemon
+    button.innerText = pokemon.name
+    //Class is added for CSS styling
+    button.classList.add('button-class')
+
+    //allows buttons and list to show on webpage
+    listItem.appendChild(button)
+    pokemonList.appendChild(listItem)
+
+    //When buttons are clicked the pokemons names are logged
+    button.addEventListener('click', function (event) {
+    })
+  }
+  function showDetails (pokemon) {
+    console.log(pokemon)
+  }
+
+  //Returns an object with all functions assigned as keys
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails
+  }
+})()
 
 //forEach loop going over objects in array starting with index 0
 pokemonRepository.getAll().forEach(function (pokemon) {
-   pokemonRepository.addListItem(pokemon);
-});
-
-
+  pokemonRepository.addListItem(pokemon)
+})
