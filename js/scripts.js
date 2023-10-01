3b47e8e3755a4cb65ffe58d4084eddc3b11bbd26
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-10-01 17:46:31 -0600

    replace pokemon array with empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,6 @@
 let pokemonRepository = (function () {
   //array of pokemon objects
-  let pokemonList = [
-    {
-      name: 'Blastoise',
-      type: ['Water'],
-      height: 1.6
-    },
-
-    {
-      name: 'Umbreon',
-      type: ['Dark'],
-      height: 1
-    },
-
-    {
-      name: 'Eevee',
-      type: ['Normal'],
-      height: 0.3
-    },
-
-    {
-      name: 'Gengar',
-      type: ['Ghost', 'Poison'],
-      height: 1.5
-    }
-  ]
+  let pokemonList = [ ]
 
   //function to return pokemonList-array
   function getAll () {
