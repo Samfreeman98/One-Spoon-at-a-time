37737f7c5f575e64c21a981650cb278d43a1cb49
Samfreeman98 <140463087+Samfreeman98@users.noreply.github.com>
2023-10-01 19:28:18 -0600

    update format

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,38 @@
 let pokemonRepository = (function () {
-  //array of pokemon objects
-  let pokemonList = [ ]
+  let pokemonList = [];
+  //pokemon API
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
   //function to return pokemonList-array
-  function getAll () {
-    return pokemonList
+  function getAll() {
+    return pokemonList;
   }
 
-  function add (pokemon) {
-    pokemonList.push(pokemon)
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
 
-  function addListItem (pokemon) {
+  function addListItem(pokemon) {
     //selecting the pokemon-list
-    let pokemonList = document.querySelector('.pokemon-list')
+    let pokemonList = document.querySelector(".pokemon-list");
 
     //Creating variables for list items and buttons.
-    let listItem = document.createElement('li')
-    let button = document.createElement('button')
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
 
     //Button text is the name of the pokemon
-    button.innerText = pokemon.name
+    button.innerText = pokemon.name;
     //Class is added for CSS styling
-    button.classList.add('button-class')
+    button.classList.add("button-class");
 
     //allows buttons and list to show on webpage
-    listItem.appendChild(button)
-    pokemonList.appendChild(listItem)
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
 
     //When buttons are clicked the pokemons names are logged
-    button.addEventListener('click', function (event) {
-      showDetails(pokemon)
-    })
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
   }
   function showDetails (pokemon) {
     console.log(pokemon)
@@ -48,8 +49,8 @@ let pokemonRepository = (function () {
 
   //Returns an object with all functions assigned as keys
   return {
-    getAll: getAll,
     add: add,
+    getAll: getAll,
     addListItem: addListItem,
     showDetails: showDetails
   }
